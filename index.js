ecf9364fa9dc70b346904b72200ab43dcd7422a8
Tomás Ruiz <tomasruizr@gmail.com>
2019-07-26 19:42:54 -0300

    [remove] extend createNew mixin

--- index.js
+++ index.js
@@ -1,45 +1,3 @@
-function getClassProto(classProto) {
-	let proto = {};
-	Object.getOwnPropertyNames(classProto).forEach((prop) => {
-		if (prop !== 'constructor') {
-			proto[prop] = classProto[prop];
-		}
-	});
-	return proto;
-}
-// TODO: Take a look into constructor property
-function createNew(constructors, ...args) {
-	console.warn('DEPRECATION WARNING: trutils.createNew is deprecated');
-	let newObj = {};
-	let protos = [];
-	if (!Array.isArray(constructors)) constructors = [constructors];
-	if (args.length && (constructors.length > 1 && (args.length !== 1 || typeof args[0] !== 'object'))) {
-		throw new Error('Arguments should be an object protoname-based for each prototype');
-	}
-	let params;
-	for (let index = 0; index < constructors.length; index++) {
-		if (typeof constructors[index] === 'function') {
-			params = constructors.length === 1 ? args : args[0][constructors[index].name];
-			params = !Array.isArray(params) ? [params] : params;
-			if (/^class/.test(constructors[index].prototype.constructor)) {
-				let auxObj = new constructors[index](...params);
-				Object.getOwnPropertyNames(auxObj).forEach(prop => {
-					newObj[prop] = auxObj[prop];
-				});
-				protos.push(getClassProto(constructors[index].prototype));
-			} else {
-				constructors[index].apply(newObj, params);
-				protos.push(constructors[index].prototype);
-			}
-		} else {
-			protos.push(constructors[index]);
-		}
-	}
-	let prototype = Object.assign({}, ...protos);
-	Object.setPrototypeOf(newObj, prototype);
-	return newObj;
-}
-
 function monad(obj, objectPath, nValue, params = {}) {
 	const {arrayPush, doNotCreate} = params;
 	const parts = Array.isArray(objectPath) ? objectPath : objectPath.split('.');
@@ -103,31 +61,11 @@ function findKey(obj, key) {
 	}
 }
 
-function mixin(top, ...funcs) {
-	console.warn('DEPRECATION WARNING: trutils.mixin is deprecated');
-	top.prototype = Object.assign({}, ...(funcs.map(f=>f.prototype)), top.prototype);
-	Object.defineProperty(top.prototype, 'constructor', {
-		enumerable: false,
-		value: top
-	});
-}
-
-function extend(subProto, baseProto) {
-	console.warn('DEPRECATION WARNING: trutils.extend is deprecated');
-	subProto.prototype = Object.create(baseProto.prototype);
-	Object.defineProperty(subProto.prototype, 'constructor', {
-		enumerable: false,
-		value: subProto
-	});
-}
 
 
 module.exports = {
-	createNew,
 	mExists,
 	mGet,
 	mSet,
 	findKey,
-	mixin,
-	extend
 };
