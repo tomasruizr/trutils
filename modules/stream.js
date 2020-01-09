5b01cb5fbf3e365c2f25def291f0a108613e32ba
Tomas Ruiz <tomasruizr@gmail.com>
2020-01-09 01:00:18 -0300

    [add] flyd-skip

--- modules/stream.js
+++ modules/stream.js
@@ -2,6 +2,7 @@ const f = require( 'flyd' );
 const filter = require( 'flyd/module/filter' );
 const once = require( 'flyd-once' );
 const bufferCount = require( 'flyd-buffercount' );
+const skip = require( 'flyd-skip' );
 const { ifElse, prop, tryCatch } = require( './fp.js' );
 
 function sComposeChain( fnsArray, params = {}) {
@@ -20,6 +21,22 @@ function sComposeChain( fnsArray, params = {}) {
   return result;  
 }
 
+function sForEach( fn, array ){
+  const result = f.stream();
+  const sArray = f.stream();
+  f.endsOn( result, sArray );
+  const resolveValue = ifElse( prop( 'then' ), 
+    ( promise )=>promise.then( sArray ).catch( result ),
+    sArray
+  );
+  sArray.map(
+    tryCatch( ifElse(() => !!array.length, 
+      ()=>resolveValue( fn( array.pop())),
+      result
+    ),result ));
+  return result; 
+}
+
 module.exports = {
   stream: f.stream,
   sMap: f.map,
@@ -35,6 +52,7 @@ module.exports = {
   sApply: f.ap,
   sFilter: filter,
   sOnce: once,
+  sSkip: skip,
   sBufferCount: bufferCount,
-  sComposeChain
+  sComposeChain,
 };
