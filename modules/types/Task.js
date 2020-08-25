3455b17d4b3476c1cc7cf07622525827ad282460
Tomás Ruiz <tomasruizr@gmail.com>
2020-08-24 23:41:10 -0300

    [add] Custom Task module

--- modules/types/Task.js
+++ modules/types/Task.js
@@ -1,9 +1,174 @@
-const Task = require( 'data.task' );
+function Task( fork ) {
+  const map = ( mapFunction ) =>
+    Task(( reject, resolve ) =>
+      fork( reject, mapValue =>
+        resolve( mapFunction( mapValue ))));
 
+  const effect = effectFunction => 
+    Task(( reject, resolve ) =>
+      fork( reject, value => 
+        ( effectFunction( value ), resolve( value ))));
+
+  const chain = ( chainFunction ) =>
+    Task(( reject, resolve ) =>
+      fork( reject, chainValue => 
+        Task.ensureTask( chainFunction( chainValue ))
+          .fork( reject, resolve )));
+  
+  const bimap = ( rejectedMap, map = rejectedMap ) => 
+    Task(( reject, resolve ) =>
+      fork( rejectedValue => reject( rejectedMap( rejectedValue ))
+        , value => resolve( map( value ))));
+
+  const fold = ( rejectedMap, map = rejectedMap ) => 
+    Task(( _, resolve ) =>
+      fork( rejectedValue => resolve( rejectedMap( rejectedValue ))
+        , value => resolve( map( value ))));
+      
+  const rejectedMap = rejectedMap =>
+    Task(( reject, resolve ) => 
+      fork( rejectedValue => 
+        reject( rejectedMap( rejectedValue ))
+      , resolve ));
+ 
+  const rejectedChain = rejectedMap =>
+    Task(( reject, resolve ) => 
+      fork( rejectedValue => 
+        rejectedMap( rejectedValue ).fork( reject, resolve )
+      , resolve ));
+
+  const swap = () =>
+    Task(( reject, resolve ) => 
+      fork( resolve , reject ));
+
+  const ap = ( that ) => {
+    return Task( function( reject, resolve ) {
+      let theFunction;
+      let value;
+      let isValueSet;
+      let isRejected = false;
+      const guardResolve = setter => item => {
+        if ( isRejected ) return;
+        setter( item );
+        if ( theFunction != null && isValueSet ) {
+          return resolve( theFunction( value ));
+        } else {
+          return item;
+        }
+      };
+      const guardReject = x => {
+        if ( !isRejected ) {
+          isRejected = true;
+          return reject( x );
+        }
+      };
+      fork( guardReject, guardResolve( func => theFunction = func ));
+      that.fork( guardReject, guardResolve( val => {
+        value = val; 
+        isValueSet = true;
+      }));
+    });
+  };
+  
+
+  const or = ( that ) => {
+    return Task(( reject, resolve ) => {
+      let isDone = false;
+      const guard = theFunction => value => {
+        if ( isDone ) return;
+        isDone = true;
+        return theFunction( value );
+      };
+      fork( guard( reject ), guard( resolve ));
+      that.fork( guard( reject ), guard( resolve ));
+    });
+  };
+
+  
+  const and = ( that ) => { 
+    let resolved = false;
+    let rejected = false;
+    const result = [];
+    const pending = ([ first, second ] = result ) =>
+      first === undefined || second === undefined;
+    const guard = ( rej, res, i ) => [ 
+      e => resolved || rejected
+        ? undefined
+        : ( rejected = true , rej ( e )),
+      x => ( result[i] = x || [], resolved || rejected || pending ()
+        ? undefined
+        : ( resolved = true , res( result.flatMap( x=>x )))),
+    ];
+    return Task (( reject, resolve ) => ( 
+      fork( ...guard( reject, resolve, 0 )),
+      that.fork( ...guard( reject, resolve, 1 ))
+    ));
+  };
+
+  const toString = () => `Task(${fork})`;
+
+      
+  return { 
+    isTask: true,
+    map, 
+    bimap,
+    rejectedMap,
+    swap,
+    fold,
+    orElse: rejectedChain,
+    rejectedChain,
+    chain,
+    ap,
+    concat: or,
+    or,
+    and,
+    effect,
+    toString,
+    fork,
+  };
+}
+
+Task.of = Task.empty = taskValue => Task(( _, resolve ) => resolve( taskValue ));
+
+Task.rejected = function _rejected( rejectedValue ) {
+  return new Task( function( reject ) {
+    return reject( rejectedValue );
+  });
+};
+
+Task.ensureTask = ( maybeTask ) => {
+  if ( maybeTask == null ) return Task.of();
+  return maybeTask.isTask ? maybeTask : Task.fromPromise( maybeTask );
+};
 
 Task.fromPromise = ( maybePromise ) => new Task(( rej, res ) => 
   maybePromise.then ? maybePromise.then( res ).catch( rej ) : res( maybePromise ));
 
-Task.isTask = ( maybeTask ) => maybeTask instanceof Task;
+Task.isTask = ( maybeTask ) => maybeTask.isTask;
 
-module.exports = Task;
+Task.and = ( task1, task2 ) => { 
+  let resolved = false;
+  let rejected = false;
+  const result = [];
+  const pending = ([ a, b ] = result ) =>
+    a === undefined || b === undefined;
+  const guard = ( rej, res, i ) => [ 
+    e => resolved || rejected
+      ? undefined
+      : ( rejected = true , rej ( e )),
+    x => ( result[i] = x, resolved || rejected || pending ()
+      ? undefined
+      : ( resolved = true , res ( result ))),
+  ];
+  return Task (( rej, res ) => ( 
+    task1.fork ( ...guard ( rej, res, 0 )),
+    task2.fork ( ...guard ( rej, res, 1 ))
+  ));
+};
+
+Task.all = ( task, ...tasks ) =>
+  !task ? Task.of([]) :
+    Task.and( task, Task.all( ...tasks ))
+      .map(([ x, xs ]) => [ x, ...xs ]);
+
+module.exports = Task ;
--- modules/types/task.test.js
+++ modules/types/task.test.js
@@ -1,24 +1,311 @@
-const { noop, True } = require( '../functions.js' );
 const Task = require( './Task.js' );
 const { assert } = require( 'chai' );
+const sinon = require( 'sinon' );
 
 describe( 'Task', function() {
-  describe( 'ap', function() {
+  const failSafe = () => assert( false );
+  describe( 'Static properties', function() {
+    assert.exists( Task.of );
+    assert.exists( Task.rejected );
+    assert.exists( Task.isTask );
+    assert.exists( Task.ensureTask );
+    assert.exists( Task.fromPromise );
+  });
+
+  describe( 'Task', function() {
+    it( 'returns a Task for non async operation', () => {
+      const x = Task(( _, resolve ) => resolve( 'str' ));
+      x.fork( failSafe, str => assert.equal( str, 'str' ));
+    });
+    it( 'returns a Task for async operation', done => {
+      const x = Task(( _, resolve ) => setTimeout(() => resolve( 'str' ), 10 ));
+      x.fork( failSafe, str => {
+        assert.equal( str, 'str' );
+        done();
+      });
+    });
+  });
+
+  describe( 'of', function() {
+    it( 'returns an object with isTask === true', () => {
+      const x = Task.of();
+      assert.isTrue( x.isTask );
+    });
+    it( 'returns an object with possible operations', () => {
+      const x = Task.of();
+      assert.exists( x.isTask );
+      assert.exists( x.map ); 
+      assert.exists( x.bimap );
+      assert.exists( x.rejectedMap );
+      assert.exists( x.swap );
+      assert.exists( x.fold );
+      assert.exists( x.orElse );
+      assert.exists( x.chain );
+      assert.exists( x.ap );
+      assert.exists( x.concat );
+      assert.exists( x.or );
+      assert.exists( x.toString );
+      assert.exists( x.fork );
+    });
+    describe( 'task.fork ', function() {
+      it( 'forks the value of the task', () => {
+        const x = Task.of( 'some string' );
+        x.fork( failSafe, str => assert.equal( str, 'some string' ));
+      });
+    });
+  });
+
+  describe( 'rejected', function() {
+    describe( 'task.fork ', function() {
+      it( 'forks the value of the rejected task', () => {
+        const x = Task.rejected( 'some string' );
+        x.fork( str => assert.equal( str, 'some string' ), failSafe );
+      });
+    });
+  });
+
+  describe( 'and', function() {
+    it( 'executes two async operations and returns an array with the results', done => {
+      Task.and( 
+        Task(( _, resolve ) => setTimeout(() => { resolve( 1 ); }, 10 )),
+        Task(( _, resolve ) => setTimeout(() => { resolve( 2 ); }, 10 ))
+      )
+        .map( x=> { 
+          assert.deepEqual( x, [ 1,2 ]);
+          return x; 
+        }) 
+        .fork( done, () => done());
+    });
+  });
+  
+  describe( 'all', function() {
+    it( 'executes two async operations and returns an array with the results', done => {
+      Task.all( 
+        Task(( _, resolve ) => setTimeout(() => { resolve( 1 ); }, 10 )),
+        Task(( _, resolve ) => setTimeout(() => { resolve( 2 ); }, 8 )),
+        Task(( _, resolve ) => setTimeout(() => { resolve( 3 ); }, 10 )),
+        Task(( _, resolve ) => setTimeout(() => { resolve( 4 ); }, 1 )),
+        Task(( _, resolve ) => setTimeout(() => { resolve( 5 ); }, 5 ))
+      )
+        .map( x => {
+          assert.deepEqual( x, [ 1,2,3,4,5 ]);
+        })
+        .fork( done, () => done());
+    });
+  });
+
+  describe( 'task.map', function() {
+    it( 'maps the value of a task', () => {
+      Task.of( 10 )
+        .map( num => num + 5 )
+        .fork( failSafe, num => assert.equal( num, 15 ));
+    });
+    it( 'maps the value of a task', () => {
+      Task.of( 10 )
+        .map( num => num + 5 )
+        .map( num => num + 5 )
+        .map( num => num + 5 )
+        .map( num => num + 5 )
+        .fork( failSafe, num => assert.equal( num, 30 ));
+    });
+  });
+
+  describe( 'task.chain', function() {
+    it( 'chains the value of a task if chain function returns a task', () => {
+      Task.of( 10 )
+        .chain( num => Task.of( num + 5 ))
+        .fork( failSafe, num => assert.equal( num, 15 ));
+    });
+    it( 'chains the value of a task if chain function returns a non task value', () => {
+      Task.of( 10 )
+        .chain( num => num + 5 )
+        .fork( failSafe, num => assert.equal( num, 15 ));
+    });
+    it( 'chains the value of a task if chain function returns a Promise', () => {
+      Task.of( 10 )
+        .chain( num => Promise.resolve( num + 5 ))
+        .fork( failSafe, num => assert.equal( num, 15 ));
+    });
+  });
+
+  describe( 'task.bimap', function() {
+    it( 'makes a map operation if resolved', done => {
+      Task.of( 'str' )
+        .bimap( str => str.toUpperCase())
+        .map( str => {
+          assert.equal( str, 'STR' );
+        }).fork( failSafe, () => done());
+    });
+    it( 'makes a map operation if rejected', done => {
+      Task.rejected( 'str' )
+        .bimap( str => str.toUpperCase())
+        .rejectedMap( str => {
+          assert.equal( str, 'STR' );
+        }).fork(() => done(), failSafe );
+    });
+  });
+  
+  describe( 'task.fold', function() {
+    it( 'makes a rejectedMap operation if resolved', done => {
+      Task.of( 'str' )
+        .fold( str => str.toUpperCase())
+        .map( str => {
+          assert.equal( str, 'STR' );
+        }).fork( failSafe, () => done());
+    });
+    it( 'makes a map operation if resolved', done => {
+      Task.of( 'str' )
+        .fold( failSafe, str => str.toUpperCase())
+        .map( str => {
+          assert.equal( str, 'STR' );
+        }).fork( failSafe, () => done());
+    });
+    it( 'makes a map operation if rejected and resolved the result', done => {
+      Task.rejected( 'str' )
+        .fold( str => str.toUpperCase())
+        .map( str => {
+          assert.equal( str, 'STR' );
+        }).fork( failSafe, () => done());
+    });
+    it( 'makes a rejectedMap operation if rejected and resolved the result', done => {
+      Task.rejected( 'str' )
+        .fold(() => 'the rejected map op', failSafe )
+        .map( str => {
+          assert.equal( str, 'the rejected map op' );
+        }).fork( failSafe, () => done());
+    });
+  });
+
+  describe( 'task.rejectedChain', function() {
+    it( 'does nothing if resolved', done => {
+      Task.of( 'str' )
+        .orElse( str => str.toUpperCase())
+        .map( str => {
+          assert.equal( str, 'str' );
+        }).fork( failSafe, () => done());
+    });
+    it( 'makes a chain operation if rejected', done => {
+      Task.rejected( 'str' )
+        .orElse( str => Task.of( str.toUpperCase()))
+        .map( str => {
+          assert( str, 'STR' );
+        }).fork( failSafe, () => done());
+    });
+  });
+
+  describe( 'task.rejectedChain', function() {
+    it( 'does nothing if resolved', done => {
+      Task.of( 'str' )
+        .orElse( str => str.toUpperCase())
+        .map( str => {
+          assert.equal( str, 'str' );
+        }).fork( failSafe, () => done());
+    });
+    it( 'makes a chain operation if rejected', done => {
+      Task.rejected( 'str' )
+        .orElse( str => Task.of( str.toUpperCase()))
+        .map( str => {
+          assert( str, 'STR' );
+        }).fork( failSafe, () => done());
+    });
+  });
+
+  describe( 'task.swap', function() {
+    it( 'rejects a resolved task', done => {
+      Task.of( 'str' )
+        .swap()
+        .rejectedMap( str => {
+          assert.equal( str, 'str' );
+        }).fork(() => done(), failSafe );
+    });
+    it( 'resolves a rejected task', done => {
+      Task.rejected( 'str' )
+        .swap()
+        .map( str => {
+          assert.equal( str, 'str' );
+        }).fork( failSafe, () => done());
+    });
+  });
+
+  describe( 'task.ap', function() {
     it( 'rejects if one of the ap functions rejects', () => {
       Task.of( one=>two => `${one}-${two}` )
         .ap( Task.of( 'hola' ))
         .ap( Task.rejected( 'pana' ))
-        .fork( err => assert.equal( err, 'pana' ), () => assert( false ));
+        .fork( err => assert.equal( err, 'pana' ), failSafe );
     });
-    it( 'works after maps', () => {
+    it( 'works after maps for async ops', () => {
       Task.of( 'mio' )
         .map( str => str.toUpperCase())
         .map( a => b => c => a + b + c )
-        .ap( Task.of( 'hola' ))
-        .ap( Task.of( 'pana' ))
-        .fork( err => assert( false, err ), str => {
+        .ap( Task(( _, resolve ) => setTimeout(() => resolve( 'hola' )), 5 ))
+        .ap( Task(( _, resolve ) => setTimeout(() => resolve( 'pana' )), 5 ))
+        .fork( failSafe, str => {
           assert.equal( str, 'MIOholapana' );
         });
+    });
+    it( 'works for several async ops', done => {
+      const spy = sinon.spy();
+      Task.of(() => () => 'listo' )
+        .ap( new Task(( rej, resolve )=> setTimeout(() => { spy(); resolve(); }, 200 )))
+        .ap( new Task(( rej, resolve )=> setTimeout(() => { spy(); resolve(); }, 100 )))
+        .map( str => {
+          assert.equal( str, 'listo' );
+          assert.equal( spy.callCount, 2 );
+        })
+        .fork( done, () => done());
+    });
+  });
+
+  describe( 'task.and', function() {
+    it( 'executes several async operations and returns an array with the results', done => {
+      Task.of()
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 1 ); }, 10 )))
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 2 ); }, 8 )))
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 3 ); }, 6 )))
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 4 ); }, 3 )))
+        .map( x=> { 
+          assert.deepEqual( x, [ 1,2,3,4 ]);
+        }) 
+        .fork( done, () => done());
+    });
+    it( 'executes several async operations and returns an array with the results', done => {
+      Task.of( 'someThing' )
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 1 ); }, 10 )))
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 2 ); }, 8 )))
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 3 ); }, 6 )))
+        .and( Task(( _, resolve ) => setTimeout(() => { resolve( 4 ); }, 3 )))
+        .map( x=> { 
+          assert.deepEqual( x, [ 'someThing', 1,2,3,4 ]);
+        }) 
+        .fork( done, () => done());
+    });
+  });
+
+  describe( 'task.effect', function() {
+    it( 'performs a side effect operation but passes the current state to next step', done => {
+      const spy = sinon.spy();
+      Task.of( 'some data' )
+        .map( x=> x.toUpperCase())
+        .effect( spy )
+        .map( str=> {
+          assert.equal( str, 'SOME DATA' );
+          assert.isTrue( spy.called );
+          assert.isTrue( spy.calledWith( 'SOME DATA' ));
+        })
+        .fork( done, () => done());
+    });
+    it( 'performs a side effect operation but passes the current state to next step', done => {
+      const spy = sinon.spy();
+      Task.of( 'some data' )
+        .map( x=> x.toUpperCase())
+        .effect(() => spy())
+        .map( str=> {
+          assert.equal( str, 'SOME DATA' );
+          assert.isTrue( spy.called );
+          assert.deepEqual( spy.args, [[]]);
+        })
+        .fork( done, () => done());
     });
   });
 });
