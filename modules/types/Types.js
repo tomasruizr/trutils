2e4ac754943da62cf43067b31244a0abf768fc11
Tomás Ruiz <tomasruizr@gmail.com>
2020-09-02 12:40:56 -0300

    [add] queue has

--- modules/types/Types.js
+++ modules/types/Types.js
@@ -5,8 +5,9 @@ const Queue = ( init ) => {
   let values = ( init && ensureArray( init )) || [];
   return {
     get length() { return values.length ; },
-    pop() { return !!values.length && values.pop( values ); },
-    push( value ) { values.unshift( value ); return this; },
+    pop: () => !!values.length && values.pop( values ),
+    push: value => { values.unshift( value ); return Queue( values ); },
+    has: value => values.includes( value ),
     ap: data => values.map( x => x( data )),
     map: f => Queue( values.map( f )),
     fold: f => values.map( f ),
