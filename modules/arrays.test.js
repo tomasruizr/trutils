4e5073427288e330dbe57b1f1874c1f1d7aca9c1
Tomás Ruiz <tomasruizr@gmail.com>
2020-09-06 13:06:41 -0300

    [fix] array tests seek=>findAndPerform

--- modules/arrays.test.js
+++ modules/arrays.test.js
@@ -1,4 +1,4 @@
-const { range, push, unshift, seek, foldMap } = require( './arrays.js' );
+const { range, push, unshift, foldMap, findAndPerform } = require( './arrays.js' );
 const { assert } = require( 'chai' );
 
 const Sum = value =>
@@ -63,12 +63,12 @@ describe( 'arrays.js', function() {
   describe( 'seek', function() {
     it( 'finds an item in an array appliying a predicate, and returns the result of such apply over found item', () => {
       const arr = [ 2, 4 ];
-      const result = seek( x => x % 2, arr );
+      const result = findAndPerform( x => x % 2, arr );
       assert.isUndefined( result );
     });
     it( 'finds an item in an array appliying a predicate, and returns the result of such apply over found item', () => {
       const arr = [ 2, 4, 3 ];
-      const result = seek( x => x % 2, arr );
+      const result = findAndPerform( x => x % 2, arr );
       assert.equal( result, 1 );
     });
   });
